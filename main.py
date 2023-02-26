9c67881d1ea2e91c9916e0bca75c315c95d5f33b
Andrey314IT <125789209+Andrey314IT@users.noreply.github.com>
2023-02-26 07:25:24 +0000

    Added cat

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ class Person:
     self.name = name
     self.height = height
 
-class Cat
+class Cat:
+  color = "Black"
+  height = 40
+  age = 1 
+  name = "Choky"
+  def _init__(self, height, age, name, color):
+    self.name = name
+    self.height = height
+    self.age = age
+    self.color = color
+
+
 me = Person("Andrey", 170)
 friend = Person("Igor", 168)
+cat = Cat("Choky")
